e3dabac4c0aab69ff4ff51b2198f29043c852350
Khoa Pham <kenjifam@gmail.com>
2020-10-12 21:41:59 +0300

    add browser history

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,7 @@ import { setConfig } from 'react-hot-loader';
 
 import { CssBaseline } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
-import App from './App';
+import AppRouter from './AppRouter';
 
 import theme from './assets/styles/theme';
 import store from './redux';
@@ -18,23 +18,16 @@ setConfig({
 const AppWithStyles = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    <AppRouter />
   </ThemeProvider>
 );
 
-const render = () =>
-  ReactDOM.render(
-    <Provider store={store}>
-      <AppWithStyles />
-    </Provider>,
-    document.getElementById('root'),
-  );
-
-render();
-
-if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./App', render);
-}
+ReactDOM.render(
+  <Provider store={store}>
+    <AppWithStyles />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
