6ccd3684803f9e7ffe4da82f7d35ea70fc3e6e31
Khoa Pham <kenjifam@gmail.com>
2020-11-13 21:02:00 +0200

    auth reducer and actions

--- client/src/redux/actions/AuthActions.ts
+++ client/src/redux/actions/AuthActions.ts
@@ -1,3 +1,93 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
+import { AxiosResponse } from 'axios';
+import axios, { setTokenHeader, saveToken, getToken, removeToken } from '../../services/api';
+import { ReduxState } from '..';
+import { LoginFormData, SignUpFormData, User } from '../../services/types';
+import history from '../../services/history';
+import { fetchStorages, updateStorages } from './StoragesActions';
+import { fetchFoods, updateFoods } from './FoodsActions';
 
-export const userLogout = createAction('USER_LOGOUT');
+export const authReset = createAction('Auth/AUTH_RESET');
+
+export const updateUser = createAction<User>('Auth/UPDATE_USER');
+
+export const userLogout = createAsyncThunk<void, void, { state: ReduxState }>(
+  'Auth/USER_LOGOUT',
+  async (_, { dispatch }) => {
+    removeToken();
+    setTokenHeader();
+    dispatch(authReset());
+    dispatch(updateStorages([]));
+    dispatch(updateFoods([]));
+    history.push('/login');
+  },
+);
+
+export const userLogin = createAsyncThunk<User, LoginFormData, { state: ReduxState }>(
+  'Auth/USER_LOGIN',
+  async (loginFormData: LoginFormData, { dispatch }) => {
+    try {
+      const auth: AxiosResponse<{ user: User; token: string }> = await axios.post(
+        `/auth/login`,
+        loginFormData,
+      );
+      const { token, user } = auth.data;
+      setTokenHeader(token);
+      saveToken(token);
+      dispatch(fetchStorages());
+      dispatch(fetchFoods());
+      history.push('/');
+      return user;
+    } catch (e) {
+      throw new Error('Wrong email or password!');
+    }
+  },
+);
+
+export const userSignUp = createAsyncThunk<User, SignUpFormData, { state: ReduxState }>(
+  'Auth/USER_SIGNUP',
+  async (signupFormData: SignUpFormData, { dispatch }) => {
+    try {
+      const auth: AxiosResponse<{ user: User; token: string }> = await axios.post(
+        `/auth/signup`,
+        signupFormData,
+      );
+      const { token, user } = auth.data;
+      setTokenHeader(token);
+      saveToken(token);
+      dispatch(fetchStorages());
+      dispatch(fetchFoods());
+      history.push('/');
+      return user;
+    } catch (e) {
+      throw new Error('Register failed!');
+    }
+  },
+);
+
+export const initialAuth = createAsyncThunk<User | null, void, { state: ReduxState }>(
+  'Auth/INIT_AUTH',
+  async (_, { getState, dispatch }) => {
+    const token = getToken();
+    const encodedTokenPayload = token?.split('.')[1];
+    const decodedTokenPayload = encodedTokenPayload ? JSON.parse(atob(encodedTokenPayload)) : null;
+    const now = Date.now() / 1000;
+    const tokenValid =
+      !!decodedTokenPayload?.id &&
+      parseInt(decodedTokenPayload.iat) < now &&
+      parseInt(decodedTokenPayload.exp) > now;
+    if (token && tokenValid) {
+      const { data } = getState().auth;
+      if (data) {
+        return data;
+      }
+      setTokenHeader(token);
+      const response: AxiosResponse<User> = await axios.get('/user');
+      dispatch(fetchStorages());
+      dispatch(fetchFoods());
+      return response.data;
+    } else {
+      return null;
+    }
+  },
+);
--- client/src/redux/reducers/AuthReducer.ts
+++ client/src/redux/reducers/AuthReducer.ts
@@ -1,25 +1,70 @@
 import { createReducer } from '@reduxjs/toolkit';
+import { User } from '../../services/types';
 import * as AuthActions from '../actions/AuthActions';
 
 export interface AuthState {
-  data: /* User | */ null;
+  data: User | null;
   loading: boolean;
+  initialLoading: boolean;
   error: string | null;
 }
 
 const INITIAL_STATE: AuthState = {
   data: null,
   loading: false,
+  initialLoading: true,
   error: null,
 };
 
 const AuthReducer = createReducer(INITIAL_STATE, (builder) => {
-  builder.addCase(AuthActions.userLogout, (state, action) => ({
-    ...state,
-    data: null,
-    loading: false,
-    error: null,
-  }));
+  builder.addCase(AuthActions.authReset, (state, action) => {
+    state.data = null;
+    state.error = null;
+    state.loading = false;
+  });
+
+  builder.addCase(AuthActions.updateUser, (state, action) => {
+    state.data = action.payload;
+  });
+
+  // User login
+  builder.addCase(AuthActions.userLogin.pending, (state, action) => {
+    state.loading = true;
+  });
+  builder.addCase(AuthActions.userLogin.fulfilled, (state, action) => {
+    state.data = action.payload;
+    state.loading = false;
+  });
+  builder.addCase(AuthActions.userLogin.rejected, (state, action) => {
+    state.error = action.error.message ?? '';
+    state.loading = false;
+  });
+
+  // User signup
+  builder.addCase(AuthActions.userSignUp.pending, (state, action) => {
+    state.loading = true;
+  });
+  builder.addCase(AuthActions.userSignUp.fulfilled, (state, action) => {
+    state.data = action.payload;
+    state.loading = false;
+  });
+  builder.addCase(AuthActions.userSignUp.rejected, (state, action) => {
+    state.error = action.error.message ?? '';
+    state.loading = false;
+  });
+
+  // Initial auth
+  builder.addCase(AuthActions.initialAuth.pending, (state, action) => {
+    state.initialLoading = true;
+  });
+  builder.addCase(AuthActions.initialAuth.fulfilled, (state, action) => {
+    state.data = action.payload;
+    state.initialLoading = false;
+  });
+  builder.addCase(AuthActions.initialAuth.rejected, (state, action) => {
+    state.error = action.error.message ?? '';
+    state.initialLoading = false;
+  });
 });
 
 export default AuthReducer;
