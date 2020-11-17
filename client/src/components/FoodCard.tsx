1aafd3d1327c6c3f63b8dfe45bec134fe6f369fa
Khoa Pham <kenjifam@gmail.com>
2020-11-17 21:37:08 +0200

    edit food card

--- client/src/components/FoodCard.tsx
+++ client/src/components/FoodCard.tsx
@@ -1,66 +1,177 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
 import classNames from 'classnames';
 import { lighten, makeStyles } from '@material-ui/core/styles';
-import { Box, Grid, Typography } from '@material-ui/core';
-import { Food } from '../services/data';
-
-export interface FoodToRender extends Food {
-  categoryName: string;
-}
+import { Box, Button, Collapse, Grid, Slider, TextField, Typography } from '@material-ui/core';
+import { Food, FoodFormData } from '../services/types';
+import { icons } from '../services/data';
+import { useDispatch, useSelector } from 'react-redux';
+import { ReduxDispatch, ReduxState } from '../redux';
+import Spinner from './Spinner';
+import { updateFood } from '../redux/actions/FoodsActions';
 
 interface Props {
-  foodToRender: FoodToRender;
+  foodToRender: Food;
 }
 
-const FoodCard = (props: Props) => {
-  const { name, brand, icon: Icon, expText, exp, amount, categoryName } = props.foodToRender;
-  const classes = useStyles();
+const getExpiringPercentage = (expiredDate: string) => {
+  const maxDuration = moment.duration(7, 'days').asHours();
+  const remainingDuration = moment(expiredDate).diff(moment(), 'hours');
+  const expiringPercentage = ((maxDuration - remainingDuration) / maxDuration) * 100;
+  return expiringPercentage > 5 ? expiringPercentage : 5;
+};
 
-  const progressbarColor =
-    exp < 0.4
-      ? classes.progressbarInnerGreen
-      : exp < 0.7
+const FoodCard = (props: Props) => {
+  const { id, name, brand, icon, amount, expiredDate, storage } = props.foodToRender;
+  const classes = useStyles();
+  const dispatch: ReduxDispatch = useDispatch();
+
+  const { loading } = useSelector((state: ReduxState) => state.foods);
+
+  const [open, setOpen] = useState(false);
+  const [amountToUse, setAmountToUse] = useState(Math.round(amount.unused / 2));
+
+  const Icon = icons.filter((ico) => ico.id === icon)[0]?.icon;
+  const remainingDays = moment(expiredDate).diff(moment(), 'day');
+  const progressbarColorClass =
+    remainingDays < 2
+      ? classes.progressbarInnerRed
+      : remainingDays < 4
       ? classes.progressbarInnerYellow
-      : classes.progressbarInnerRed;
+      : classes.progressbarInnerGreen;
+  const expText =
+    remainingDays < 1
+      ? 'Expiring today'
+      : `${moment.duration(remainingDays, 'days').humanize({ d: 8, w: 4 })} left`;
+
+  const onSubmit = async () => {
+    const formData: Partial<FoodFormData> = {
+      amount: {
+        ...amount,
+        unused: amount.unused - amountToUse,
+      },
+    };
+    const result = await dispatch(updateFood({ foodId: id, data: formData }));
+    if (updateFood.fulfilled.match(result)) {
+      setOpen(false);
+    }
+  };
+
+  const renderFoodInfo = () => (
+    <Grid
+      container
+      wrap='nowrap'
+      alignContent='center'
+      className={classes.cardGridContainer}
+      onClick={() => setOpen(!open)}
+    >
+      <Grid item className={classes.cardGridIcon}>
+        <Box className={classes.cardIcon}>
+          <Icon width={24} height={24} />
+        </Box>
+      </Grid>
+      <Grid container justify='space-between' alignContent='center'>
+        <Box>
+          <Typography variant='body2' color='primary' className={classes.fontBold}>
+            {name}
+          </Typography>
+          {brand && (
+            <Typography variant='caption' color='primary' component='p'>
+              {brand}
+            </Typography>
+          )}
+          <Typography
+            variant='caption'
+            color={remainingDays < 2 ? 'error' : 'inherit'}
+            component='p'
+          >
+            {expText}
+          </Typography>
+        </Box>
+        <Box display='flex' justifyContent='center' flexDirection='column'>
+          <Typography variant='body2' color='primary' className={classes.fontBold}>
+            {`${amount.unused} ${amount.unit}`}
+          </Typography>
+          <Typography
+            variant='caption'
+            color='primary'
+            align='right'
+            className={classes.categoryName}
+          >
+            {storage.name}
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+
+  const renderEditFood = () => (
+    <Collapse in={open} timeout='auto' unmountOnExit>
+      <Box className={classes.editBox}>
+        <Typography
+          variant='body2'
+          color='primary'
+          align='center'
+          gutterBottom
+          className={classes.fontBold}
+        >
+          Select amount to use
+        </Typography>
+        <Box display='flex' alignItems='center'>
+          <TextField
+            className={classes.amountField}
+            value={amountToUse}
+            onChange={(e) => setAmountToUse(Number(e.target.value))}
+            type='number'
+            required
+            InputProps={{
+              disableUnderline: true,
+              classes: {
+                root: classes.inputRoot,
+                input: classes.input,
+              },
+            }}
+          />
+          <Typography variant='body2' color='primary' component='span' className={classes.fontBold}>
+            {amount.unit}
+          </Typography>
+        </Box>
+        <Box py={1} px={2}>
+          <Slider
+            value={amountToUse}
+            max={amount.unused}
+            onChange={(_, value) => setAmountToUse(value as number)}
+            classes={{
+              thumb: classes.sliderThumb,
+              active: classes.active,
+            }}
+          />
+        </Box>
+        <Button variant='contained' color='primary' fullWidth onClick={onSubmit} disabled={loading}>
+          {loading ? <Spinner size={24} color='white' /> : `Use ${name.toLowerCase()}`}
+        </Button>
+      </Box>
+    </Collapse>
+  );
+
+  const renderProgressbar = () => (
+    <Box className={classNames(classes.progressbar, open && classes.progressbarExpand)}>
+      <Box
+        className={classNames(
+          classes.progressbarInner,
+          progressbarColorClass,
+          open && classes.progressbarInnerExpand,
+        )}
+        style={{ width: `${getExpiringPercentage(expiredDate)}%` }}
+      />
+    </Box>
+  );
 
   return (
     <Box className={classes.cardWrapper}>
-      <Grid container wrap='nowrap' alignContent='center' className={classes.cardGridContainer}>
-        <Grid item className={classes.cardGridIcon}>
-          <Box className={classes.cardIcon}>
-            <Icon width={24} height={24} />
-          </Box>
-        </Grid>
-        <Grid container justify='space-between' alignContent='center'>
-          <Box>
-            <Typography variant='body2' color='primary' className={classes.fontBold}>
-              {name}
-            </Typography>
-            {brand && (
-              <Typography variant='caption' color='primary' component='p'>
-                {brand}
-              </Typography>
-            )}
-            <Typography variant='caption' color='error' component='p'>
-              {expText}
-            </Typography>
-          </Box>
-          <Box display='flex' justifyContent='center' flexDirection='column'>
-            <Typography variant='body2' color='primary' className={classes.fontBold}>
-              {`${amount.value} ${amount.unit}`}
-            </Typography>
-            <Typography variant='caption' color='primary' className={classes.categoryName}>
-              {categoryName}
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
-      <Box className={classes.progressbar}>
-        <Box
-          className={classNames(classes.progressbarInner, progressbarColor)}
-          style={{ width: `${exp * 100}%` }}
-        />
-      </Box>
+      {renderEditFood()}
+      {renderProgressbar()}
+      {renderEditFood()}
     </Box>
   );
 };
@@ -71,13 +182,13 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: 13,
     boxShadow: '0 17px 23px -13px #e6e6e6, 0 2px 24px 0 rgba(0, 0, 0, 0.12)',
     marginBottom: theme.spacing(1),
-    height: 84,
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
     position: 'relative',
   },
   cardGridContainer: {
-    height: '100%',
+    height: 77,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    cursor: 'pointer',
   },
   cardGridIcon: {
     display: 'flex',
@@ -97,21 +208,30 @@ const useStyles = makeStyles((theme) => ({
     opacity: 0.8,
   },
   progressbar: {
-    position: 'absolute',
     bottom: 0,
-    left: 0,
-    right: 0,
+    left: 1.47,
+    right: 1.47,
     height: 7,
+    transition: 'margin 300ms, border-radius 300ms',
     backgroundColor: '#d8d8d8',
-    borderBottomLeftRadius: 13,
-    borderBottomRightRadius: 13,
+    borderRadius: '0 0 13px 13px / 0 0 7px 7px',
+  },
+  progressbarExpand: {
+    borderRadius: 7,
+    margin: theme.spacing(0, 2, 2),
+    transition: 'margin 300ms, border-radius 300ms',
   },
   progressbarInner: {
     height: 7,
-    borderBottomLeftRadius: 13,
+    borderRadius: '0 0 0 13px / 0 0 0 7px',
+    transition: 'border-radius 300ms',
+  },
+  progressbarInnerExpand: {
+    borderRadius: 7,
+    transition: 'border-radius 300ms',
   },
   progressbarInnerRed: {
-    backgroundColor: theme.palette.error.main,
+    backgroundColor: theme.palette.error.light,
   },
   progressbarInnerYellow: {
     backgroundColor: theme.palette.warning.light,
@@ -121,6 +241,41 @@ const useStyles = makeStyles((theme) => ({
   },
   fontBold: {
     fontWeight: 500,
+  },
+  editBox: {
+    padding: theme.spacing(2),
+    borderTop: `1px solid ${theme.palette.divider}`,
+  },
+  amountField: {
+    marginLeft: 'calc(50% - 46px)',
+    marginRight: theme.spacing(1),
+  },
+  inputRoot: {
+    border: `1px solid ${theme.palette.grey['500']}`,
+    padding: theme.spacing(0.375, 1.25),
+    color: theme.palette.primary.main,
+    fontWeight: 500,
+    maxWidth: 92,
+  },
+  input: {
+    textAlign: 'center',
+    [`&::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button`]: {
+      '-webkit-appearance': 'none',
+      margin: 0,
+    },
+  },
+  active: {},
+  sliderThumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: theme.palette.background.paper,
+    border: `1px solid ${theme.palette.primary.main}`,
+    marginTop: -11,
+    marginLeft: -12,
+    '&:focus, &:hover, &$active': {
+      boxShadow: 'inherit',
+    },
   },
 }));
 
