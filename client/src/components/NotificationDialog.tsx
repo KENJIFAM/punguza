c53a6790346ce7be2dc8d3fa41e9979fe1453df9
Khoa Pham <kenjifam@gmail.com>
2020-11-20 16:39:14 +0200

    update notification dialog

--- client/src/components/NotificationDialog.tsx
+++ client/src/components/NotificationDialog.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import moment from 'moment';
+import { useSelector } from 'react-redux';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import {
   Dialog,
@@ -10,7 +12,7 @@ import {
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/CloseRounded';
 import NotificationCategory from './NotificationCategory';
-import { notifications } from '../services/data';
+import { ReduxState } from '../redux';
 
 interface Props {
   open: boolean;
@@ -22,6 +24,17 @@ const NotificationDialog = (props: Props) => {
   const classes = useStyles();
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('xs'));
+
+  const { usableFoods, expiredFoods, historyFoods: _historyFoods } = useSelector(
+    (state: ReduxState) => state.foods.data,
+  );
+
+  const expiringFoods = usableFoods.filter((f) => moment(f.expiredDate).diff(moment(), 'day') < 2);
+  const historyFoods = [..._historyFoods].sort((a, b) => {
+    const dateToCompareA = a.throwed ? a.expiredDate : a.updatedAt;
+    const dateToCompareB = b.throwed ? b.expiredDate : b.updatedAt;
+    return moment(dateToCompareA).isBefore(moment(dateToCompareB)) ? 1 : -1;
+  });
 
   return (
     <Dialog
@@ -41,13 +54,9 @@ const NotificationDialog = (props: Props) => {
         </Icon>
       </DialogTitle>
       <DialogContent className={classes.dialogContent}>
-        {Object.entries(notifications).map(([cat, notifications]) => (
-          <NotificationCategory
-            key={cat}
-            notifications={notifications}
-            notificationCategoryId={cat}
-          />
-        ))}
+        <NotificationCategory foods={expiringFoods} notificationCategory='expiring' />
+        <NotificationCategory foods={expiredFoods} notificationCategory='expired' />
+        <NotificationCategory foods={historyFoods} notificationCategory='history' />
       </DialogContent>
     </Dialog>
   );
